296c8e57f8488d58d9d594b4ed31521aaf45237c
Sean Palmer <seantpalmer@gmail.com>
2015-07-30 04:03:13 -0600

    Implemented context sensitive camera position dampening.

--- AAG/Assets/Scrips/CameraController.cs
+++ AAG/Assets/Scrips/CameraController.cs
@@ -5,61 +5,84 @@ public class CameraController : MonoBehaviour {
 	public float vCameraRotation = 45;
 	public float hCameraRotation = 0;
 	public float restingDistance = 30;
+	public float restingAngle = 45;
 	public float upperCameraLimit = 90;
 	public float lowerCameraLimit = -90;
+	public float hSensitivity = 32;
+	public float vSensitivity = 18;
 	public LayerMask layerMask = 1 << 8;
 
-	Vector3 playerPos;
+	Vector2 axis1;
+	Vector2 axis2;
+	float playerPosX;
+	float playerPosY;
+	float playerPosZ;
 	float camPosX;
 	float camPosY;
 	float camPosZ;
 	float camDist;
 	float rence;
-	Rigidbody rigi;
+	float renceH;
+	float renceV;
+	Transform tran;
 	Transform camTran;
 	RaycastHit hit;
 	// Use this for initialization
 	void Start () {
-		rigi = gameObject.GetComponent <Rigidbody> ();
+		tran = gameObject.GetComponent <Transform> ();
 		camTran = Camera.main.GetComponent <Transform> ();
 		camDist = restingDistance;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		playerPosX = tran.position.x;
+		playerPosY = tran.position.y;
+		playerPosZ = tran.position.z;
+		axis1 = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		axis2 = new Vector2 (Input.GetAxisRaw ("Horizontal2"), Input.GetAxisRaw ("Vertical2"));
+
 		if (hCameraRotation > 360) {
 			hCameraRotation -= 360;
 		}
 		if (hCameraRotation < 0) {
 			hCameraRotation += 360;
 		}
+			
+		if (Physics.Raycast (tran.position, camTran.position - tran.position, out hit, restingDistance, layerMask)) {
+			camDist = Mathf.SmoothDamp(camDist, hit.distance, ref rence, 0.1f);
+		} else {
+			camDist = Mathf.SmoothDamp(camDist, restingDistance, ref rence, 0.6f);
+		}
+
+
+		vCameraRotation += axis2.y * vSensitivity * Time.deltaTime;
+
+		if (axis2.sqrMagnitude == 0) {
+			hCameraRotation = Mathf.SmoothDampAngle (hCameraRotation, tran.eulerAngles.y, ref renceH, 0.55f);
+			vCameraRotation = Mathf.SmoothDampAngle (vCameraRotation, restingAngle, ref renceV, 0.2f);
+		} else {
+			hCameraRotation += axis2.x * hSensitivity * Time.deltaTime;
+			vCameraRotation += axis2.y * vSensitivity * Time.deltaTime;
+		}
+
 		if (vCameraRotation > upperCameraLimit) {
 			vCameraRotation = upperCameraLimit;
 		} 
 		if (vCameraRotation < lowerCameraLimit) {
 			vCameraRotation = lowerCameraLimit;
 		} 
-			
-		if (Physics.Raycast (rigi.position, camTran.position - rigi.position, out hit, restingDistance, layerMask)) {
-			camDist = Mathf.SmoothDamp(camDist, hit.distance, ref rence, 0.1f);
-		} else {
-			camDist = Mathf.SmoothDamp(camDist, restingDistance, ref rence, 0.6f);
-		}
 
-		playerPos = rigi.position;
-		//camPosX = playerPos.x + restingDistance * Mathf.Sin (-hCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad);
-		//camPosY = playerPos.y + restingDistance * Mathf.Sin (vCameraRotation * Mathf.Deg2Rad);
-		//camPosZ = playerPos.z - restingDistance * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (-hCameraRotation * Mathf.Deg2Rad);
+		camPosX = playerPosX + camDist * Mathf.Sin (-hCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad);
+		camPosY = playerPosY + camDist * Mathf.Sin (vCameraRotation * Mathf.Deg2Rad);
+		camPosZ = playerPosZ - camDist * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (-hCameraRotation * Mathf.Deg2Rad);
 
-		camPosX = playerPos.x + camDist * Mathf.Sin (-hCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad);
-		camPosY = playerPos.y + camDist * Mathf.Sin (vCameraRotation * Mathf.Deg2Rad);
-		camPosZ = playerPos.z - camDist * Mathf.Cos (vCameraRotation * Mathf.Deg2Rad) * Mathf.Cos (-hCameraRotation * Mathf.Deg2Rad);
+		camTran.eulerAngles = new Vector3 (vCameraRotation, hCameraRotation, 0);
 
 		camTran.position = new Vector3 (camPosX, camPosY, camPosZ);
-		camTran.eulerAngles = new Vector3 (vCameraRotation, hCameraRotation, 0);
-		Debug.DrawRay (rigi.position, camTran.position - rigi.position);
-	}
 
+	}
+	// FixedUpdate is called fifty times per second
 	void FixedUpdate () {
 
 	}
--- AAG/Assets/Scrips/Controller.cs
+++ AAG/Assets/Scrips/Controller.cs
@@ -4,8 +4,6 @@ using System.Collections;
 public class Controller : MonoBehaviour {
 	public float moveSpeed = 10;
 	public float jumpHight = 10;
-	public float hSensitivity = 16;
-	public float vSensitivity = 9;
 
 	float cRotation = 0;
 	float xMovement;
@@ -19,12 +17,12 @@ public class Controller : MonoBehaviour {
 	Vector3 angles;
 	Transform tran;
 	Rigidbody rigi;
-	CameraController camCon;
+	Transform camTran;
 	// Use this for initialization	
 	void Start () {
 		tran = gameObject.GetComponent <Transform> ();
 		rigi = gameObject.GetComponent <Rigidbody> ();
-		camCon = gameObject.GetComponent <CameraController> ();
+		camTran = Camera.main.gameObject.GetComponent <Transform> ();
 	}
 
 	// Update is called once per frame	
@@ -38,20 +36,18 @@ public class Controller : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
-		cRotation = camCon.hCameraRotation;
+		cRotation = camTran.eulerAngles.y;
 		angles = tran.eulerAngles;
 		axis = Vector2.ClampMagnitude (new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")), 0.8f);
 		xMovement = axis.x * Mathf.Cos (cRotation  * Mathf.Deg2Rad) + axis.y  * Mathf.Sin (cRotation * Mathf.Deg2Rad);
 		zMovement = axis.y * Mathf.Cos (cRotation * Mathf.Deg2Rad) - axis.x * Mathf.Sin (cRotation  * Mathf.Deg2Rad); 
 
-		if (Input.GetAxisRaw ("Horizontal") != 0 || Input.GetAxisRaw ("Vertical") != 0) {
+		if (axis.sqrMagnitude!= 0) {
 			inputDirection = Mathf.Atan2 (xMovement, zMovement) * Mathf.Rad2Deg;
 			yAngle = Mathf.SmoothDampAngle (angles.y, inputDirection, ref rence, (0.96f - axis.sqrMagnitude)/2);
 			rigi.MoveRotation (Quaternion.AngleAxis (yAngle, Vector3.up));
 		}
 
-		camCon.hCameraRotation += Input.GetAxisRaw ("Horizontal2") * hSensitivity;
-		camCon.vCameraRotation += Input.GetAxisRaw ("Vertical2") * vSensitivity;
 		rigi.AddForce (new Vector3(xMovement, 0, zMovement) * moveSpeed );
 	}
 }
